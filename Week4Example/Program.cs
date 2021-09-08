f7e2ac554cb7a800ff0a6af40ce6b020ff8a5724
Johnnie Odom <jlodom00@alumni.amherst.edu>
2021-09-07 19:07:09 -0500

    Class 5 Notes on previous work.

--- Week4Example/Program.cs
+++ Week4Example/Program.cs
@@ -14,13 +14,18 @@ namespace Week4Example
             Boolean boolNotFirstRow = false;
             List<String> listStringHeaders = new List<string>();
             List<Dictionary<String, String>> spreadsheet = new List<Dictionary<string, string>>();
+            /* Class 5 - We are ready to discuss loops. 
+             * Taking each string (line) in the stringArrayAllLines collection (array) and doing something with it.
+             * Computers are good at REPEATING instructions with DIFFERENT DATA. */
             foreach (String line in stringArrayAllLines)
             {
+                /* DECISION - If/Else */
                 if (boolNotFirstRow)
                 {
                     Dictionary<String, String> dicStringTemp = new Dictionary<string, string>();
                     String[] stringArrayData = line.Split('|');
                     int i = 0;
+                    /* NESTED REPEATING LOOP (i.e. another foreach inside our first foreach) */
                     foreach (String stringHeader in listStringHeaders)
                     {
                         dicStringTemp.Add(stringHeader, stringArrayData[i]);
@@ -38,9 +43,9 @@ namespace Week4Example
 
             /* This is where we start using objects. */
             BudgetCategory bcLegal = new BudgetCategory(spreadsheet[13]);
-            Console.WriteLine("The percent different for 2018-2019 in legal was " 
+            Console.WriteLine("The percent different for 2018-2019 in legal was "
                 + bcLegal.GetPercent1819());
-           
+
         }
 
     }
