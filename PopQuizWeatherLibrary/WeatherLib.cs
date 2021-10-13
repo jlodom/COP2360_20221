6256b864e81272fd4cec17a7564dae66343b4dfd
Johnnie Odom <jlodom00@alumni.amherst.edu>
2021-10-12 19:15:42 -0500

    Starting to use a library

---
+++ PopQuizWeatherLibrary/WeatherLib.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace PopQuizWeatherLibrary
+{
+    public class WeatherLib
+    {
+
+    }
+}
---
+++ PopQuizWeatherTester/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using PopQuizWeatherLibrary;
+
+namespace PopQuizWeatherTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
