457b3e8007b71a82a160ec323f5730e19de66f1f
Johnnie Odom <jlodom00@alumni.amherst.edu>
2021-10-26 20:01:51 -0500

    Added program to illustrate how command line might be used with Project 1.

---
+++ CommandLineExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace CommandLineExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String candidateName = args[0]; /* Get Candidate Name From Argument 1 */
+            String pathToInputFile = args[1]; /* Get Path to File From Argument 2 */
+
+            string[] allLines = File.ReadAllLines(pathToInputFile);
+
+            Console.WriteLine("First ten lines of " + pathToInputFile);
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(allLines[i]);
+            }
+
+            Console.WriteLine("Our candidate name is: " + candidateName);
+        }
+
+    }
+}
